d0ab396858e949ee0e7f2d1c5aeebe9f56256f49
fikrinurfazri <fikrinurfazri93@gmail.com>
2023-02-27 11:10:06 +0700

    update

--- application/views/pages/admin/footer.php
+++ application/views/pages/admin/footer.php
@@ -1,11 +1,12 @@
 <footer class="site-footer">
+
+
     <div class="site-footer-bottom">
         <div class="container">
             <div class="row">
 
                 <div class="col-lg-6 col-md-7 col-12">
-                    <p class="copyright-text mb-0">Copyright © 2036 <a href="#">Kind Heart</a> Charity Org.
-                        Design: <a href="https://templatemo.com" target="_blank">TemplateMo</a></p>
+                    <p class="copyright-text mb-0">Copyright © 2023 <a href="#">BAG.ORGANISASI</a> PEMKKOT TASIKMALAYA.</p>
                 </div>
 
                 <div class="col-lg-6 col-md-5 col-12 d-flex justify-content-center align-items-center mx-auto">
@@ -45,15 +46,6 @@
 <script src="<?= base_url() ?>assets/js/counter.js"></script>
 <script src="<?= base_url() ?>assets/js/custom.js"></script>
 
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src=" https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-<script src=" https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
-
-<script>
-    $(document).ready(function() {
-        $('#tabel-data').DataTable();
-    });
-</script>
 
 </body>
 
--- application/views/pages/footer.php
+++ application/views/pages/footer.php
@@ -6,7 +6,7 @@
             <div class="row">
 
                 <div class="col-lg-6 col-md-7 col-12">
-                    <p class="copyright-text mb-0">Copyright © 2023 <a href="#">BAG.ORGANISASI</a> PEMKKOT TASIKMALAYA.</p>
+                    <p class="copyright-text mb-0">Copyright © 2023 <a href="#">BAG.ORGANISASI</a> PEMKOT TASIKMALAYA.</p>
                 </div>
 
                 <div class="col-lg-6 col-md-5 col-12 d-flex justify-content-center align-items-center mx-auto">
